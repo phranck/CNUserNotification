73549d08c32f78fcc78082c48eb37c147405de38
Frank Gregor <phranck@cocoanaut.com>
2013-10-08 23:22:42 +0200

    Changed: cleanup

--- CNUserNotification/CNUserNotificationCenter.m
+++ CNUserNotification/CNUserNotificationCenter.m
@@ -31,12 +31,15 @@
 #import "CNUserNotificationBannerController.h"
 
 
-NSString *kCNUserNotificationDismissDelayTimeKey = @"com.cocoanaut.userNotification.dismissDelayTime";
-NSString *kCNUserNotificationBannerArchivedImageKey = @"com.cocoanaut.userNotification.bannerImage";
-NSString *kCNUserNotificationBannerLineBreakModeKey = @"com.cocoanaut.userNotification.bannerLineBreakMode";
+NSString *const CNUserNotificationDismissDelayTimeKey = @"CNUserNotificationDismissDelayTimeKey";
+NSString *const CNUserNotificationBannerArchivedImageKey = @"CNUserNotificationBannerArchivedImageKey";
+NSString *const CNUserNotificationBannerLineBreakModeKey = @"CNUserNotificationBannerLineBreakModeKey";
 
-NSString *CNUserNotificationDismissBannerNotification = @"com.cocoanaut.userNotification.dismissBanner";
-NSString *CNUserNotificationActivatedWithTypeNotification = @"com.cocoanaut.userNotification.activatedWithType";
+NSString *const CNUserNotificationDismissBannerNotification = @"CNUserNotificationDismissBannerNotification";
+NSString *const CNUserNotificationActivatedWithTypeNotification = @"CNUserNotificationActivatedWithTypeNotification";
+
+NSString *const CNUserNotificationDefaultSound = @"CNUserNotificationDefaultSound";
+NSString *const NSUserNotificationDefaultSoundName = @"NSUserNotificationDefaultSoundName";
 
 
 @interface CNUserNotificationCenter () {}
@@ -49,95 +52,87 @@ NSString *CNUserNotificationActivatedWithTypeNotification = @"com.cocoanaut.user
 #pragma clang diagnostic ignored "-Wswitch"
 @implementation CNUserNotificationCenter
 
-+ (instancetype)defaultUserNotificationCenter
-{
-    __strong static id sharedInstance = nil;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        if (NSClassFromString(@"NSUserNotificationCenter")) sharedInstance = [NSUserNotificationCenter defaultUserNotificationCenter];
-        else sharedInstance = [[[self class] alloc] init];
-    });
-    return sharedInstance;
++ (instancetype)defaultUserNotificationCenter {
+	__strong static id sharedInstance = nil;
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+	    if (NSClassFromString(@"NSUserNotificationCenter")) sharedInstance = [NSUserNotificationCenter defaultUserNotificationCenter];
+	    else sharedInstance = [[[self class] alloc] init];
+	});
+	return sharedInstance;
 }
 
-+ (instancetype)customUserNotificationCenter
-{
-    __strong static id sharedInstance = nil;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{ sharedInstance = [[[self class] alloc] init]; });
-    return sharedInstance;
++ (instancetype)customUserNotificationCenter {
+	__strong static id sharedInstance = nil;
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{ sharedInstance = [[[self class] alloc] init]; });
+	return sharedInstance;
 }
 
-- (id)init
-{
-    self = [super init];
-    if (self) {
-        _notificationBannerController = nil;
-        _cn_scheduledNotifications = [[NSMutableArray alloc] init];
-    }
-    return self;
+- (id)init {
+	self = [super init];
+	if (self) {
+		_notificationBannerController = nil;
+		_cn_scheduledNotifications = [[NSMutableArray alloc] init];
+	}
+	return self;
 }
 
-- (void)deliverNotification:(CNUserNotification *)notification
-{
-    CNUserNotificationBannerActivationHandler activationHandler = ^(CNUserNotificationActivationType activationType) {
-        [[NSNotificationCenter defaultCenter] postNotificationName:CNUserNotificationActivatedWithTypeNotification object:@(activationType)];
-        switch (activationType) {
-            case CNUserNotificationActivationTypeContentsClicked:
-            case CNUserNotificationActivationTypeActionButtonClicked: {
-                CNUserNotificationCenter *center = [CNUserNotificationCenter defaultUserNotificationCenter];
-                if ([self userNotificationCenter:center shouldPresentNotification:notification]) {
-                    [self userNotificationCenter:center didActivateNotification:notification];
-                }
-                break;
-            }
-        }
-    };
+- (void)deliverNotification:(CNUserNotification *)notification {
+	CNUserNotificationBannerActivationHandler activationHandler = ^(CNUserNotificationActivationType activationType) {
+		[[NSNotificationCenter defaultCenter] postNotificationName:CNUserNotificationActivatedWithTypeNotification object:@(activationType)];
+		switch (activationType) {
+			case CNUserNotificationActivationTypeContentsClicked:
+			case CNUserNotificationActivationTypeActionButtonClicked: {
+				CNUserNotificationCenter *center = [CNUserNotificationCenter defaultUserNotificationCenter];
+				if ([self userNotificationCenter:center shouldPresentNotification:notification]) {
+					[self userNotificationCenter:center didActivateNotification:notification];
+				}
+				break;
+			}
+		}
+	};
 
-    self.notificationBannerController = nil;
-    self.notificationBannerController = [[CNUserNotificationBannerController alloc] initWithNotification:notification
-                                                                                                delegate:self.delegate
-                                                                                usingActivationHandler:activationHandler];
-    /// inform the delegate
-    [self userNotificationCenter:self didDeliverNotification:notification];
+	self.notificationBannerController = nil;
+	self.notificationBannerController = [[CNUserNotificationBannerController alloc] initWithNotification:notification
+	                                                                                            delegate:self.delegate
+	                                                                              usingActivationHandler:activationHandler];
+	// inform the delegate
+	[self userNotificationCenter:self didDeliverNotification:notification];
 
-    [self.notificationBannerController presentBannerDismissAfter:notification.feature.dismissDelayTime];
+	[self.notificationBannerController presentBannerDismissAfter:notification.feature.dismissDelayTime];
 
-    if (notification.soundName != nil) {
-        if ([notification.soundName isEqualToString:NSUserNotificationDefaultSoundName]) {
-            [[NSSound soundNamed:@"CNUserNotificationDefaultSound"] play];
-        } else {
-            [[NSSound soundNamed:notification.soundName] play];
-        }
-    }
+	if (notification.soundName != nil) {
+		if ([notification.soundName isEqualToString:CNUserNotificationDefaultSound]) {
+			[[NSSound soundNamed:CNUserNotificationDefaultSound] play];
+		}
+		else {
+			[[NSSound soundNamed:notification.soundName] play];
+		}
+	}
 }
 
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #pragma mark - CNUserNotificationCenter Delegate
 
-- (void)userNotificationCenter:(CNUserNotificationCenter *)center didDeliverNotification:(CNUserNotification *)notification
-{
-    if ([self.delegate respondsToSelector:_cmd]) {
-        [self.delegate userNotificationCenter:center didDeliverNotification:notification];
-    }
+- (void)userNotificationCenter:(CNUserNotificationCenter *)center didDeliverNotification:(CNUserNotification *)notification {
+	if ([self.delegate respondsToSelector:_cmd]) {
+		[self.delegate userNotificationCenter:center didDeliverNotification:notification];
+	}
 }
 
-- (BOOL)userNotificationCenter:(CNUserNotificationCenter *)center shouldPresentNotification:(CNUserNotification *)notification
-{
-    BOOL shouldPresent = NO;
-    if ([self.delegate respondsToSelector:_cmd]) {
-        shouldPresent = [self.delegate userNotificationCenter:center shouldPresentNotification:notification];
-    }
-    return shouldPresent;
+- (BOOL)userNotificationCenter:(CNUserNotificationCenter *)center shouldPresentNotification:(CNUserNotification *)notification {
+	BOOL shouldPresent = NO;
+	if ([self.delegate respondsToSelector:_cmd]) {
+		shouldPresent = [self.delegate userNotificationCenter:center shouldPresentNotification:notification];
+	}
+	return shouldPresent;
 }
 
-- (void)userNotificationCenter:(CNUserNotificationCenter *)center didActivateNotification:(CNUserNotification *)notification
-{
-    if ([self.delegate respondsToSelector:_cmd]) {
-        [self.delegate userNotificationCenter:center didActivateNotification:notification];
-        [[NSNotificationCenter defaultCenter] postNotificationName:CNUserNotificationDismissBannerNotification object:nil];
-    }
+- (void)userNotificationCenter:(CNUserNotificationCenter *)center didActivateNotification:(CNUserNotification *)notification {
+	if ([self.delegate respondsToSelector:_cmd]) {
+		[self.delegate userNotificationCenter:center didActivateNotification:notification];
+		[[NSNotificationCenter defaultCenter] postNotificationName:CNUserNotificationDismissBannerNotification object:nil];
+	}
 }
 
 @end
